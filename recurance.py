de8ffdd3988419d1d7b55e0d33e69584416c697e
Bram Ratz <bratz@uoguelph.ca>
2020-06-23 17:11:22 -0400

    Reworked strategy for a list

--- recurance.py
+++ recurance.py
@@ -7,37 +7,25 @@ rabbit pairs that will be present after n months. Assumes starting with
 one male and one female baby rabbit (ie. a pair), each month once maturaity is 
 reached. Rabbits live for m months before they die.
 """
-from collections import defaultdict
 
 n = 6 # Total number of months 
 m = 3 # Months before death
 
-alive = defaultdict(int) # initialize default dictionary
+def mortalRabbits (nMonths: int, mLive: int) -> int:
+    """
+    Given the number the total number of months the simulation should be 
+    run and the number of months each rabbit has left to live, returns 
+    an integer with the number of living rabbits after nMonths.
+    """
+    # Create list of length mLive to keep track of how many living rabbits 
+    #   are still in the simulation
+    alive = [0] * mLive
+    
+    # Always start with one rabbit 
+    alive[-1] = [1]
+    
+    #
 
-
-def newNumAdults(adultsPrevRound: int, babiesPrevRound: int) -> int:
-    """Calculates the number of adults in the current round"""
-    return (adultsPrevRound + babiesPrevRound)
-
-# Starting values 
-Adults = 0 
-Babies = 0
-
-# Skips month 1 since the change from month 1 - 2 is 0 as the 1 baby pair
-# in the first month simply matures to an adult by month 2, no reproduction
-# takes place between 1 and 2 months 
-for i in range(n-1):
-    if i == 0:
-        Adults += 1
-    else:
-        newAdults = newNumAdults(Adults, Babies) # New num of adults 
-        newBabies = Adults
-        
-        # Update counts
-        Adults = newAdults
-        Babies = newBabies
-        
 # Calculate total num pairs in final round  
 totalPairs = Adults + Babies
 print(f"The total number of pairs is: {totalPairs}")
-
